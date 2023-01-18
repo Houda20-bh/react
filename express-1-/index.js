78c032701dfaeec07dad9004925bdcf9bc02d24a
Houda20-bh <bh.houda14@gmail.com>
2023-01-18 19:09:07 +0100

    node.js expressjs 1

---
+++ express-1-/index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+// router.use(function (req, res, next) {
+//     const date = new Date();
+//     if(date.getDay > 0 && date.getDay < 6 && date.getHours() > 12 && date.getHours < 17){
+//         next();
+//     }
+// });
+
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+router.get('/contact',function(req,res){
+  res.sendFile(path.join(__dirname+'/contact.html'));
+});
+
+router.get('/services',function(req,res){
+  res.sendFile(path.join(__dirname+'/services.html'));
+});
+
+app.use('/', router);
+app.listen(3000);
+
+console.log('Running at Port 3000');
