d23a8516ab2daeee4f18ef56b9cf8ee5156c1087
Houda20-bh <bh.houda14@gmail.com>
2023-01-20 01:46:49 +0100

    node.js expressjs calculator

---
+++ express-calculator/app.js
@@ -0,0 +1,23 @@
+
+const express = require ('express');
+const path = require ('path');
+const bodyParser= require('body-parser');
+ // intialize
+ const app= express();
+
+ app.use(bodyParser.urlencoded({extended:false}));
+ // home route
+ app.get('/',(req,res)=>{
+res.sendFile(path.join(__dirname,'./views/index.html'))
+ });
+ // result route
+ app.post("/", (req,res)=>{
+    const sum = Number (req.body.num1 )+ Number (req.body.num2);
+    res.send('<h4><i>your resulat is:</i></h4>'+'<h3>' + sum +'</h3>');
+ });
+ // listen to the server
+
+ app.listen (8000, ()=>{ 
+    console.log('our server running on port 8000 .... lol')
+ });
+
