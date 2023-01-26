ff2b68eeec31d38e54832fc9b35558c19251810c
Houda20-bh <bh.houda14@gmail.com>
2023-01-26 10:28:00 +0100

    Mongoose connect to atlas data

--- check-mongoose/index.js
+++ check-mongoose/index.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose');
-const Person = require("./Person")
-mongoose.connect("mongodb+srv://mango:WDKvZtMxyBD4WKKT @cluster0.q8dghnx.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('strictQuery', false);
+const Person = require("./Person");
+const dbURI= "mongodb+srv://mango:WDKvZtMxyBD4WKKT@cluster0.q8dghnx.mongodb.net/myFirstData?retryWrites=true&w=majority"
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then((result)=>console.log('connected to db'))
+.catch((erro)=>console.log('erro'));
 //Create and Save a Record of a Model
 const person = new Person({ name:"Bilel",age:9, favoriteFoods:['pizza']});
  person.save();
- console.log(person)
+ console.log(person);
+ //Create Many Records with model.create()
 const arrayOfPeople =[
-    {name:'bolbol', age:9, favoriteFoods:['burger']},
-    {name:'houda', age:33, favoriteFoods:['chocolat']},
-    {name:'Aboudi', age:6, favoriteFoods:['Pizza']},
-    {name:'aaa', age:8, favoriteFoods:['Cake']},
-]
+    {name:'bolbol', age:9, favoriteFoods:['burger','pizza','juice']},
+    {name:'houda', age:33, favoriteFoods:['chocolat','cake']},
+    {name:'Aboudi', age:6, favoriteFoods:['Pizza','rice']},
+    {name:'aaa', age:8, favoriteFoods:['Cake','fruits']},
+];
 
-   Person.create(arrayOfPeople);
+   const people= Person.create(arrayOfPeople);
+
+   console.log(people);
+   //Use model.find() to Search Your Database
+  const people1= Person.find({name:"Bilel"});
+  console.log(people1);
+  //Use model.findById() to Search Your Database By _id
+   const people2= Person.findById("63d2471d81932d7e82e23dd7");
+  console.log(people2);
+// //Perform Classic Updates by Running Find, Edit, then Save
+
+   
+
+
+   
    
 
